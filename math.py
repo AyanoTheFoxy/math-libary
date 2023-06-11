638e7aed5f9db1c083c8e051119e92f2e95fb867
Foxy <122460205+AyanoTheFoxy@users.noreply.github.com>
2023-06-10 21:03:44 -0300

    Atualizar o math.py
    now yes -b

--- math.py
+++ math.py
@@ -1,53 +1,50 @@
 #operations
-class operations:
+class math:
     def root( r: float, i: float = 2, e: float = 1) -> float:
         return r ** (e / i)
     
-    def log( l: float, b: float = 2) -> float:
+    def log( log: float, b: float = 2) -> float:
         x = 0
-        while b ** x < l:
+        while b ** x < log:
             x += 0.1
-            if b ** x >= l:
-                return round( x, 2)
+            if b ** x >= log:
+                return x
     
     def Sqdeq( a: float, b: float, c: float) -> float:
         d = b ** 2 - 4 * a * c
-        x1 = (-b - operations.root(d)) / (2 * a)
-        x2 = (-b + operations.root(d)) / (2 * a)
-        return x1, x2
-
-#positions
-class pos:
-    def vect2d( x: int = 0, y: int = 0) -> int:
-        return pos = [ x, y]
+        return (-b - math.root(d)) / (2 * a), (-b + math.root(d)) / (2 * a)
     
-    def vect3d( x: int = 0, y: int = 0, z: int = 0) -> int:
-        return pos = [ x, y]
-    
+    def pi():
+        return 3.14159265358979323846264338327950288419716939937510582097494459230781640
+        
     def lerp ( s0: int, s1: int, a: int = .5) -> int:
-        if a > 1:
-            a = 1
         return s0 + (s1 - s0) * a
-
-class area:
-    def square( x: int, y: int, w: int, h: int) -> int:
-        return area = [ x, y, w, h]
-    
-    def triangle( x: int, y: int, w: int, h: int) -> int:
-        return area = [ x * 1.5, y * 2, w / 1.5, h / 2]
-    
-    def cricle( x: int, y: int, w: int, h : int) -> int:
-        return area = [ x * 1.25, y * 1.25, w / 1.25, h / 1.25]
-    
+        
     def cmfr(r: int) -> int:
-        return 2 * 3.14159265358979323846 * r
+        return 2 * math.pi() * r
+        
+    #position
+    def vect2d( x: int, y: int) -> int:
+        return [ x, y]
     
-#conversion
-class conversions:
-    #pixels
-    class pixels:
+    def vect3d( x: int, y: int, z: int) -> int:
+        return [ x, y, z]
+        
+    class hitbox:
+        def square( x: int, y: int, w: int, h: int) -> int:
+            return [ x, y, w, h]
+    
+        def triangle( x: int, y: int, w: int, h: int) -> int:
+            return x * 1.5, y * 2, w / 1.5, h / 2
+    
+        def cricle( x: int, y: int, w: int, h : int) -> int:
+            return x * 1.25, y * 1.25, w / 1.25, h / 1.25
+    
+    #conversion
+    class conversions:
+        #pixels
         def ptToPx(pt: float) -> float:
-        return pt * .75
+            return pt * .75
     
         def emToPx(em: float) -> float:
             return em / 16
@@ -56,12 +53,11 @@ class conversions:
             return In * 120
     
         def mmToaPx(mm: float) -> float:
-            return mm * 3.7795280352161
+           return mm * 3.7795280352161
     
-    #time
-    class time:
-        def mToM(h: float) -> float:
-            return hour * 60
+        #time
+        def hToM(h: float) -> float:
+            return h * 60
         
         def hToS(h: float) -> float:
             return h * 3600
@@ -78,8 +74,6 @@ class conversions:
         def sToH(s: float) -> float:
             return s / 3600
         
-    #temp
-    class temp:
         def cToK(c: float) -> float:
             return c + 273.15
         
@@ -97,4 +91,4 @@ class conversions:
         
         def kToC(k: float) -> float:
             return k - 273.15
-        
+            
